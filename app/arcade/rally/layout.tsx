1f5adec59a22f3b91d2f509869b6ebd085340759
Justin Formella <justin.formella@gmail.com>
2026-04-04 22:53:01 -0500

    feat: add Rally-X style maze driving mini-game at /arcade/rally

---
+++ app/arcade/rally/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rally | CLCC Arcade",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
